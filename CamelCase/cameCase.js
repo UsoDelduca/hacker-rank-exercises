89a2ad7d73aabd08aa32a80e136358a4f86c0b67
Rodolfo Delduca <usodelduca@gmail.com>
2023-11-08 12:52:01 +1300

    attempt done - does not work on HackerRank

--- CamelCase/cameCase.js
+++ CamelCase/cameCase.js
@@ -1,5 +1,6 @@
 function processData(input) {
   //Check the operation (S or C)
+  let trimInput = input.substring(4)
   if (input[0] === 'S') {
     //remove unnecessary characters
     //check when there is an Upper case letter
@@ -7,12 +8,11 @@ function processData(input) {
     //remove the parenthesis
     //return a string separated by a space and Lower case
     if (input[2] !== 'M') {
-      let trimInput = input.substring(4, input.length)
       let splitInput = trimInput.split(/(?=[A-Z])/)
       return console.log(splitInput.join(' ').toLowerCase())
     }
-    let trimInput = input.substring(4, input.length - 2)
-    let splitInput = trimInput.split(/(?=[A-Z])/)
+    let trimInput2 = trimInput.substring(0, trimInput.length - 2)
+    let splitInput = trimInput2.split(/(?=[A-Z])/)
     return console.log(splitInput.join(' ').toLowerCase())
   }
   if (input[0] === 'C') {
@@ -21,7 +21,7 @@ function processData(input) {
     //make the firts letter of the following word Upper case
     //add a pair of parenthesis at the end
     //return the method name as a string
-    let trimInput = input.substring(4, input.length)
+    let trimInput = input.substring(4)
     let i = 0
     while (true) {
       i = 0
