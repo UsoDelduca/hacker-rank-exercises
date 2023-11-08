f71103d6a197b0fcc1e48cbfae017faa10e5fdd7
Rodolfo Delduca <usodelduca@gmail.com>
2023-11-08 15:11:37 +1300

    code lint

--- matchingStrings/matchingStrings.js
+++ matchingStrings/matchingStrings.js
@@ -1,16 +1,10 @@
 function matchingStrings(strings, queries) {
   // function should return an array of matching queries
-  // n = strings.length
-  // q = queries.length
-
-  // let n = strings.length
-  // let q = queries.length
-  // let count = 0
 
   // loop the queries array
   // loop the string array
   // check if queries[i] matches strings[i]
-  // when there is a match, update counter
+  // when there is a match, update the result array
 
   return queries.map(
     (string) => strings.filter((query) => query === string).length
@@ -20,5 +14,3 @@ function matchingStrings(strings, queries) {
 console.log(
   matchingStrings(['aba', 'baba', 'aba', 'xzxb'], ['aba', 'xzxb', 'ab'])
 )
-
-// (matchingStrings(['def', 'de', 'fgh'], ['de', 'lmn', 'fgh']))
