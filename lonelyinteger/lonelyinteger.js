cb2d3ebdf1a9bbdcdb5d6dd7c60edb79686f1431
Rodolfo Delduca <usodelduca@gmail.com>
2023-11-08 16:16:49 +1300

    first attempt - failed

--- lonelyinteger/lonelyinteger.js
+++ lonelyinteger/lonelyinteger.js
@@ -1,6 +1,7 @@
 function lonelyinteger(a) {
   // Write your code here
-  console.log(a)
+  let medianIdx = Math.floor(a.length / 2)
+  console.log(a[medianIdx])
 }
 
 lonelyinteger([1, 2, 3, 4, 3, 2, 1])
