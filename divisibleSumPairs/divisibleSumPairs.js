99ac0514e50b4dead433d1ea5e3a6d1f55c8b82e
Rodolfo Delduca <usodelduca@gmail.com>
2023-11-08 14:45:50 +1300

    fix typo

--- divisibleSumPairs/divisibleSumPairs.js
+++ divisibleSumPairs/divisibleSumPairs.js
@@ -2,8 +2,8 @@ function divisibleSumPairs(n, k, ar) {
   // n = array length
   // k = any positive integer
   let count = 0
-  for (let i = 0; i < ar.length; i++) {
-    for (let j = i + 1; j < ar.length; j++) {
+  for (let i = 0; i < n; i++) {
+    for (let j = i + 1; j < n; j++) {
       if ((ar[i] + ar[j]) % k === 0) {
         count += 1
       }
