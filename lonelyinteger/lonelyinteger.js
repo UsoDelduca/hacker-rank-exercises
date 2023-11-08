d35386e857ba2fc506a2dbc775b893c461c5bd24
Rodolfo Delduca <usodelduca@gmail.com>
2023-11-08 16:24:24 +1300

    second attempt - failed

--- lonelyinteger/lonelyinteger.js
+++ lonelyinteger/lonelyinteger.js
@@ -1,7 +1,17 @@
 function lonelyinteger(a) {
   // Write your code here
-  let medianIdx = Math.floor(a.length / 2)
-  console.log(a[medianIdx])
+  let n = a.length
+
+  for (let i = 0; i < n; i++) {
+    for (let j = 1; j < n; j++) {
+      if (a[i] === a[j]) {
+        console.log(`it's a match!!! ${a[i]}, ${a[j]}`)
+      } else {
+        return console.log(a[i])
+      }
+    }
+  }
 }
 
 lonelyinteger([1, 2, 3, 4, 3, 2, 1])
+// lonelyinteger([1, 1, 2])
