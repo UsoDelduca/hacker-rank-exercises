8ffa8b346872e452b4ebcd55c10de2598ab96441
Rodolfo Delduca <usodelduca@gmail.com>
2023-11-08 15:06:11 +1300

    first attempt

--- matchingStrings/matchingStrings.js
+++ matchingStrings/matchingStrings.js
@@ -1,7 +1,26 @@
 function matchingStrings(strings, queries) {
-  // Write your code here
+  // n = strings.length
+  // q = queries.length
+
+  let n = strings.length
+  let q = queries.length
+  let count = 0
+
+  // loop the queries array
+  // loop the string array
+  // check if queries[i] matches strings[i]
+  // when there is a match, update counter
+
+  for (let i = 0; i < n; i++) {
+    for (let j = 0; j < q; j++) {
+      if (queries[j] === strings[i]) {
+        count += 1
+      }
+    }
+    console.log(count)
+  }
 }
 
-matchingStrings([aba, baba, aba, xzxb], [aba, xzxb, ab])
+matchingStrings(['aba', 'baba', 'aba', 'xzxb'], ['aba', 'xzxb', 'ab'])
 
-matchingStrings([def, de, fgh], [de, lmn, fgh])
+// (matchingStrings(['def', 'de', 'fgh'], ['de', 'lmn', 'fgh']))
