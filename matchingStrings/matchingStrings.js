8c81cc6747de06917b89e2e77b219f2d90c59994
Rodolfo Delduca <usodelduca@gmail.com>
2023-11-08 14:53:47 +1300

    sets up the challenge

---
+++ matchingStrings/matchingStrings.js
@@ -0,0 +1,7 @@
+function matchingStrings(strings, queries) {
+  // Write your code here
+}
+
+matchingStrings([aba, baba, aba, xzxb], [aba, xzxb, ab])
+
+matchingStrings([def, de, fgh], [de, lmn, fgh])
