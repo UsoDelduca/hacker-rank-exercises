20f6cd8f0203848546afa38ec82c92253b3d9800
Rodolfo Delduca <usodelduca@gmail.com>
2023-11-08 14:40:24 +1300

    function done

--- divisibleSumPairs/divisibleSumPairs.js
+++ divisibleSumPairs/divisibleSumPairs.js
@@ -2,12 +2,14 @@ function divisibleSumPairs(n, k, ar) {
   // n = array length
   // k = any positive integer
   let count = 0
-  for (let i = 0; i <= n; i++) {
-    if ((ar[i] + ar[i + 1]) % k === 0) {
-      count++
+  for (let i = 0; i < ar.length; i++) {
+    for (let j = i + 1; j < ar.length; j++) {
+      if ((ar[i] + ar[j]) % k === 0) {
+        count += 1
+      }
     }
-    console.log('Count of pairs: ', count)
   }
+  return console.log(count)
 }
 
 divisibleSumPairs(6, 3, [1, 3, 2, 6, 1, 2])
